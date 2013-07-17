424e5a1ae932dd2ce24fd6f7e874fc0fe20487e9
missmarymorgan <missmarymorgan@gmail.com>
2013-07-17 09:16:57 -0400

    Create Custom Post "Reviews"
    created "reviews" custom post type

--- wp-content/themes/twentytwelve-child/functions.php
+++ wp-content/themes/twentytwelve-child/functions.php
@@ -2,4 +2,40 @@
 
 
 
+/**********
+This sets up the custom post 'Reviews'*******/
+
+function my_custom_post_review() {
+	$labels = array(
+		'name'               => _x( 'Reviews', 'reviews' ),
+		'singular_name'      => _x( 'Review', 'review' ),
+		'add_new'            => _x( 'Add New', 'review' ),
+		'add_new_item'       => __( 'Add New Review' ),
+		'edit_item'          => __( 'Edit Review' ),
+		'new_item'           => __( 'New Review' ),
+		'all_items'          => __( 'All Reviews' ),
+		'view_item'          => __( 'View Review' ),
+		'search_items'       => __( 'Search Reviews' ),
+		'not_found'          => __( 'No reviews found' ),
+		'not_found_in_trash' => __( 'No reviews found in the Trash' ), 
+		'parent_item_colon'  => '',
+		'menu_name'          => 'Reviews'
+	);
+	$args = array(
+		'labels'        => $labels,
+		'description'   => 'Reviews from the greater DC area',
+		'public'        => true,
+		'menu_position' => 5,
+		'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+		'has_archive'   => true,
+	);
+	register_post_type( 'review', $args );	
+}
+add_action( 'init', 'my_custom_post_review' );
+
+
+
+
+
+
 ?>
