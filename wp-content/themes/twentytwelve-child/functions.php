f15e39c18f9ab1be4555042a009052b876b3d698
missmarymorgan <missmarymorgan@gmail.com>
2013-07-20 14:08:22 -0400

    "Continue Reading" link
    Add "continue reading" link to home page excerpts so that the whole
    damn text isn't there.

--- wp-content/themes/twentytwelve-child/functions.php
+++ wp-content/themes/twentytwelve-child/functions.php
@@ -104,6 +104,15 @@ add_action( 'init', 'my_custom_post_howTo' );
 
 
 
+/* Replaces the excerpt "more" text by a link*/
+
+
+function new_excerpt_more($more) {
+       global $post;
+	return '<a class="moretag" href="'. get_permalink($post->ID) . '"> Read the full article...</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+
 
 
 
