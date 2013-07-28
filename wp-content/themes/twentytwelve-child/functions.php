c1f2f1b2888859f6807d2fe8b4b90854ac65f029
missmarymorgan <missmarymorgan@gmail.com>
2013-07-28 16:34:26 -0400

    Add contact form; Removed 'about' and 'contact' from home page
    Added a contact page to the 'about me' tab.
    
    Removed the 'about mmm' and 'contact mmm' from the home feed so that
    it's just actual posts in the feed.

--- wp-content/themes/twentytwelve-child/functions.php
+++ wp-content/themes/twentytwelve-child/functions.php
@@ -7,13 +7,13 @@ add_filter( 'pre_get_posts', 'my_get_posts' );
 function my_get_posts( $query ) {
 
 	if ( ( is_home() && $query->is_main_query() ) || is_feed() )
-		$query->set( 'post_type', array( 'post', 'page', 'review', 'lesson' ) );
+		$query->set( 'post_type', array( 'post', 'review', 'lesson' ) );
 
 	return $query;
 }
 
 
-
+/*add 'page', if want all pages to home feed in above code ^^^^ */
 
 
 /**********
